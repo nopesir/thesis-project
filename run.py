79e35370637a2d5815b6cff993e0e0a21e9d21b0
Luigi Ferrettino <s254300@studenti.polito.it>
2020-10-09 11:24:00 +0200

    modified in order to retrieve the best detection added thresh of .25

--- run.py
+++ run.py
@@ -14,42 +14,38 @@ data_file = "./darknet/thesis.data"
 weights_file = "./darknet/yolov4-thesis_last.weights"
 
 # Load image for processing
-image = wrapper.load_image(b'./2.jpg', 0, 0)
+image = wrapper.load_image(b'./4.jpg', 0, 0)
 
-image.w
 # Load network and weights
 network, class_names, colors = wrapper.load_network(config_file, data_file, weights_file)
 
 # Perform the detection
-detections = wrapper.detect_image(network, ['Car'], image) 
-
-print(wrapper.bbox2points(detections[0][2]))
-xmin, ymin, xmax, ymax = wrapper.bbox2points(detections[0][2])
+detections = wrapper.detect_image(network, ['Car'], image, thresh=.25) 
+detections = sorted(detections, key=lambda x: float(x[1]))
+print(detections)
+print(image.w)
+print(image.h)
+print(wrapper.bbox2points(detections[len(detections)-1][2]))
+xmin, ymin, xmax, ymax = wrapper.bbox2points(detections[len(detections)-1][2])
 if xmin < 0:
     xmin = 0
 
-#if xmax > (detections[0][2][0]-1):
-#    xmax = (detections[0][2][0]-1)
+if xmax > image.w:
+    xmax = image.w
 
-if ymax < 0:
-    ymax = 0
+if ymin < 0:
+    ymin = 0
 
-#if ymin > (detections[0][2][1]-1):
-#    ymin = (detections[0][2][1]-1)
+if ymax > image.h:
+    ymax = image.h
 
 print(xmin, ymin, xmax, ymax)
 center = (int((xmax+xmin)/2), int((ymax+ymin)/2))
 print(center)
 # if(left < 0) left = 0; if(right > im.w-1) right = im.w-1; if(top < 0) top = 0; if(bot > im.h-1) bot = im.h-1;
-'''
-with open('results.json') as json_file:
-    data = json.load(json_file)
 
-print(data)
 
-'''
-
-img = cv.imread('2.jpg', 0)
+img = cv.imread('4.jpg', 0)
 
 # Initiate ORB detector
 orb = cv.ORB_create()
@@ -63,6 +59,8 @@ kp, des = orb.compute(img, kp)
 # Keypoint of the center
 kp2 = cv.KeyPoint(center[0], center[1], 0)
 
+img_box = wrapper.draw_boxes(detections, img, colors)
+plt.imshow(img_box), plt.show()
 
 # Keypoints into detected object
 kp_yolo = []
@@ -81,8 +79,8 @@ for i, keypoint in enumerate(kp):
 kp_yolo.sort(key = lambda p: (p.pt[0] - kp2.pt[0])**2 + (p.pt[1] - kp2.pt[1])**2)
 
 
-kp_yolo = kp_yolo[:100]
-
+kp_yolo = kp_yolo[:20]
+img = cv.imread('4.jpg', 0)
 img2 = cv.drawKeypoints(img, kp_yolo, None, color=(255,0,0), flags=0)
 plt.imshow(img2), plt.show()
 
